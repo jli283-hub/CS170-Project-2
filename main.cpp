f58e74e17b7325c4d94d294d44692fbb4b99f3a9
jli283-hub <nmQ8Z#zG88VFyp>
2021-11-22 10:26:57 -0800

    Added nested loop search functinality

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 #include <string>
 #include <fstream>
 #include <stdio.h>
+#include <sstream>
+#include <vector>
 
 using namespace std;
+
+void feature_search_forward(string fileName, int numColumns){
+
+	vector<int> currFeatures; //initializing the current set of features to be empty
+
+	for(int i = 1; i < numColumns - 1; i++){
+		cout << "On the " << i << "th level of the search tree" << endl;
+		int feature_to_add;
+		int best_accuracy_so_far = 0;
+
+		for(int k = 1; k < numColumns - 1; k++){
+			cout << "--Considering adding the " << k << "th feature" << endl;
+		}
+	}
+
+}
+
+void feature_search_backward(string fileName, int numColumns){
+
+}
+
 
 int main(){
 
@@ -16,9 +39,55 @@ int main(){
 
 	cin >> fileName;//here i'm just going to assume that the user inputs the correct small or large text file
 
-	myFile.open(fileName);
+	myFile.open(fileName.c_str());
 
-	
+	if(myFile.is_open()){
+		cout << "FILE WAS OPENED" << endl;
 
+		cout << endl;
+
+		cout << endl;
+
+		getline(myFile,line);//grabbing the first line to count the number of columns
+
+		stringstream str; 
+
+		str << line; //sending the first line to the stringstream
+
+		int numColumns;
+
+		double currVal;
+
+		while(str >> currVal){
+			numColumns++;
+		}
+
+		myFile.close();
+
+		int option;
+
+		cout << "Please select which search algorithm you want to use." << endl;
+		cout << "1) Forward Selection" << endl;
+		cout << "2) Backward Elimination" << endl;
+
+		cout << endl;
+
+		cout << endl;
+
+		cin >> option;
+
+		cout << endl;
+
+		if(option == 1){
+			feature_search_forward(fileName,numColumns);
+		}else{
+			feature_search_backward(fileName,numColumns);
+		}
 		
+	}else{
+		cout << "FAIL!!" << endl;
+	}
+
 }
+
+
