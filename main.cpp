72e6f17f1143151af19e5639de653dd7ee3be309
jli283-hub <nmQ8Z#zG88VFyp>
2021-11-30 13:18:20 -0800

    Added statement at the end of search that displays best features and accuracy

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <sstream>
 #include <vector>
+#include <iterator>
 #include <cmath>
 #include <stdlib.h>
 #include <math.h>   
@@ -32,13 +33,36 @@ vector<double> splitString(string s){
     
 }
 
-int calculateDistance(vector<double> object1, vector<double> object2, vector<int> currFeatures, int feature_to_consider, int numColumns){
+double calculateDistance(vector<double> object1, vector<double> object2, vector<int> currFeatures, int feature_to_consider, int numColumns){
+    double distance = 9999;
+    double sum = 0;
+    vector<int> features = currFeatures;
+    
+    features.push_back(feature_to_consider); //temporarily pushing back the feature that we are considering
+    
+    for(int i = 0; i < features.size(); i++){
+        double temp1 = object1.at(features.at(i));
+        double temp2 = object2.at(features.at(i));
+        
+        sum = sum + pow(temp1 - temp2, 2);
+        
+    }
+    
+    distance = sqrt(sum);
+    
+    return distance;
 
+
+    /*
 	vector<double> instance1 = object1; //original instance
 	vector<double> instance2 = object2; //nearest neighbor that we want to calculate the distance tooi have 
 
 	double sum = 0;
 	double distance = 9999;
+    
+    */
+    
+    /*
     
         for(int k = 0; k < instance1.size(); k++){
             for(int j = 0; j < currFeatures.size(); j++){
@@ -53,25 +77,37 @@ int calculateDistance(vector<double> object1, vector<double> object2, vector<int
                 
             }
         }
+        
+    */
     
-	for(int i = 0; i < instance1.size(); i++){
-		if(i == feature_to_consider){
+    /*
+    for(int j = 0; j < currFeatures.size(); j++){
+        
+        int feature = currFeatures.at(j);
+        
+        double temp1 = instance1.at(feature);
+        double temp2 = instance2.at(feature);
+                
+        double calculation = pow(temp1 - temp2, 2);
+                
+        sum = sum + calculation;
+        
+    }
+    
             
-			double temp1 = instance1.at(i); //note: could change this to temp1 = instance.at(feature_to_consider);
-			double temp2 = instance2.at(i);
+	double temp2 = instance1.at(feature_to_consider); 
+    double temp3 = instance2.at(feature_to_consider);
             
 		
-			double calculation = pow(temp1 - temp2, 2);
+	double calculation1 = pow(temp2 - temp3, 2);
 
-			sum = sum + calculation;
-		}
-	}
-
-	
+	sum = sum + calculation1;
 
 	distance = sqrt(sum);
 
 	return distance;
+    
+    */
 	
 }
 
@@ -79,8 +115,9 @@ int calculateDistance(vector<double> object1, vector<double> object2, vector<int
 double leave_one_out_cross_validation(string fileName, vector<int> currFeatures, int feature_to_consider, int numColumns){
 
 	int number_correctly_classified = 0;
-	int nearest_neighbor_label;
-	int accuracy;
+	double nearest_neighbor_label;
+	double accuracy;
+    int sum = 0;
 	string lines;
 	ifstream infs;
 	double numRows;
@@ -102,6 +139,7 @@ double leave_one_out_cross_validation(string fileName, vector<int> currFeatures,
 			file.push_back(splitString(temp)); //storing the tokenized vector into a vector of doubles
             
 		}
+       
         
         /*
         
@@ -111,32 +149,35 @@ double leave_one_out_cross_validation(string fileName, vector<int> currFeatures,
                     cout << file[k][l] << ' ' ;
             }
             
+            cout << endl;
+            
         }
         
         */
         
         
 		for(int i = 0; i < numRows - 1; i++){ 
-			vector<double> object_to_classify = file.at(i);
+			vector<double> object_to_classify = file.at(i); //instance i
 			double label_object_to_classify = object_to_classify.at(0);
 
-			int nearest_neighbor_distance = 9999;
+			double nearest_neighbor_distance = 9999;
 			int nearest_neighbor_location = 9999;
 
 			for(int k = 0; k < numRows - 1; k++){
 				if(k != i){
 
-					int distance = calculateDistance(object_to_classify, file.at(k), currFeatures, feature_to_consider, numColumns);
+					double distance = calculateDistance(object_to_classify, file.at(k), currFeatures, feature_to_consider, numColumns);
 
-					if(distance <= nearest_neighbor_distance){
+					if(distance < nearest_neighbor_distance){
 						nearest_neighbor_distance = distance;
 						nearest_neighbor_location = k;
 						nearest_neighbor_label = file.at(k).at(0);
 					}
 				}
 			}
-
+           
 			if(label_object_to_classify == nearest_neighbor_label){
+                
 				number_correctly_classified = number_correctly_classified + 1;
 			}
 		}
@@ -148,7 +189,9 @@ double leave_one_out_cross_validation(string fileName, vector<int> currFeatures,
 
 	infs.close();
 
-	return (number_correctly_classified / (numRows - 1))* 100;
+	accuracy = (number_correctly_classified / (numRows - 1)) * 100;
+    
+    return accuracy;
 
 }
 bool isInCurrSet(vector<int> currFeatures, int k){
@@ -163,35 +206,101 @@ bool isInCurrSet(vector<int> currFeatures, int k){
 	return check; //false means that it is not in the currSet and thus needs to be considered
 }
 
+double findMaxAccuracy(vector <double> a){
+    double max = 0.0;
+    
+    for(int i = 0; i < a.size(); i++){
+        if(a.at(i) > max){
+            max = a.at(i);
+        }
+    }
+    
+    return max;
+}
+
+int findIndex(double max, vector<double> a){
+    int index;
+    
+    for(int i = 0; i < a.size(); i++){
+            if(a.at(i) == max){
+              index = i;  
+            }
+    }
+    
+    return index;
+}
+
+void printBestFeatures(vector<int> features){
+    for(int i = 0; i < features.size(); i++){
+        cout << features.at(i) << " ";
+    }
+}
+
 void feature_search_forward(string fileName, int numColumns){
 
 	vector<int> currFeatures; //initializing the current set of features to be empty
+    vector<double> accuracies; //storing the accuracies to print out the best one once search is over
+    vector<vector<int>> bestFeatures; //storing the features that were added at each level
 	double accuracy;
+    double best_accuracy_so_far;
 	int i;
 	int k;
 	for(i = 1; i < numColumns; i++){
 		cout << "On the " << i << "th level of the search tree" << endl;
 		int feature_to_add;
-		int best_accuracy_so_far = 0;
+		best_accuracy_so_far = 0.0;
 
 		for(k = 1; k < numColumns; k++){
 			if(!isInCurrSet(currFeatures, k)){ //checks if the current feature number is in the curr set of features
 
 				cout << "--Considering adding the " << k << "th feature" << endl;
 				accuracy = leave_one_out_cross_validation(fileName, currFeatures, k, numColumns); 
+                cout << fixed << setprecision(2);
 				cout << "Accuracy of feature " << k << ": " << accuracy << "%" << endl;
-				if(accuracy > best_accuracy_so_far){
+                
+                if(accuracy > best_accuracy_so_far){
 					best_accuracy_so_far = accuracy;
 					feature_to_add = k;
-				}
+                }
+                
 			}
+            
 		}
 
 		currFeatures.push_back(feature_to_add); //adding the best feature to the current set of features
+        accuracies.push_back(best_accuracy_so_far);
+        bestFeatures.push_back(currFeatures);
 		cout << "On level " << i << " I added feature " << feature_to_add << " to the current set." << endl;
         
+        cout << "Current set of Features: { ";
+        
+        copy(currFeatures.begin(),
+         currFeatures.end(),
+         ostream_iterator<int>(cout, " "));
+         
+        cout << "}" << endl;
         
 	}
+    
+    cout << endl << endl;
+    
+    cout << "---------------FINISHED SEARCH!!--------------" << endl;
+    
+   double maxAccuracy = findMaxAccuracy(accuracies);
+    
+   int max_accuracy_position = findIndex(maxAccuracy, accuracies);
+   
+   vector<int> f = bestFeatures.at(max_accuracy_position);
+    
+   cout << "The best subset of features are: { ";
+   
+   for(int k = 0; k < f.size(); k++){
+       cout << f.at(k) << " ";
+   }
+   
+   cout << "}";
+   
+   cout << " which has an accuracy of " << maxAccuracy << "%" << endl;
 
 }
 
@@ -252,9 +361,11 @@ int main(){
 
 		if(option == 1){
 			feature_search_forward(fileName,numColumns);
-		}else{
-			feature_search_backward(fileName,numColumns);
 		}
+        
+        if(option == 2){
+            feature_search_backward(fileName,numColumns);
+        }
 		
 	}else{
 		cout << "FAIL!!" << endl;
